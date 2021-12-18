59803f0b0f5cd84cc63f0380f3ad57a9dc41633f
melodytowett <melodytowett99@gmail.com>
2021-12-19 00:14:11 +0300

    add form function

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ $(document).ready(function () {
         $("#prod-showing").toggle();
         $("#prod-hidden").toggle();
     })
+    $("#Contact form").submit(function (event) {
+        var nameInput = $("input#Name").val();
+        var emailInput = $("input#Email").val();
+        var messageInput = $("input#message").val();
+        $("#Name").text(nameInput);
+        $("#Email").text(emailInput);
+        $("#message").text(messageInput);
+        event.preventDefault();
+        
+    })
+    alert(nameInput +"we have recieved your message.Thankyou for reaching out to us")
 })
